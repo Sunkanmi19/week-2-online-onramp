eea0f1f8ef23c60a6827875786ba5c8ba10c849a
Sunkanmi19 <100348387+Sunkanmi19@users.noreply.github.com>
2022-03-06 01:33:30 +0100

    that is the solution

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,18 @@
 function numSwap(number) {}
+    //coverting the number into an array
+    let numberArr = Sting(number).split("").map((number)={
+        return Number(number)
+    })
+    let hold = 0
+    for (let i = 0; i < numberArr.length; i+=2) {
+        hold = NumberArr[i]
+        numberArr[i] = numberArr[i+1]
+        numberArr[i+1] = hold
+    }
+    return Number(numberArr.join(""))
+}
+
+console.log(numSwap(1234))
+console.log(numSwap(432156))
 
 module.exports = numSwap
--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,3 +1,15 @@
-function arraySum(arr1, arr2) {}
+function arraySum(arr1, arr2) {
+    let sum [];
+    let firstlength = arr1.length;
+    let secondlength = arr2.length;
+
+    for(let i = 0; i<firstlength; i++){
+        sum[i] = arr[i] + arr2[i % secondlength];
+    }
+
+    return sum;
+}
+console.log(arraysum([1,2,4,5,6,2,1], [2,3,5]));
+console.log(arraysum([10,20,30,40,50], [20.40,60]));
 
 module.exports = arraySum
